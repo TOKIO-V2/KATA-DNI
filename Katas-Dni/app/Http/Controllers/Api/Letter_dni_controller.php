21f2ff55066269c5565d676ddd0d811143c9b03b
TOKIO-V2 <albertohernandezgaleote6@gmail.com>
2025-02-07 20:51:58 +0100

    feat: Added Controller

---
+++ Katas-Dni/app/Http/Controllers/Api/Letter_dni_controller.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Letter;
+use App\Models\letter_dni;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class LetterController extends Controller
+{
+    public function index(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'number' => 'required | integer | between:0,99999999',
+        ]);
+
+        if($validator->fails()) {
+            return response()->json([
+                'message' => 'El dato introducido no sirve',
+                'errors' => $validator->errors(),
+            ], 400);
+        }
+
+        $validated = $validator->validated();
+
+        $mod = ($validated['number'] % 23) + 1;
+
+        $result = letter_dni::findOrFail($mod);
+
+        return response()->json([
+            'letter_dni' => $result->letter,
+        ], 200);
+    }
+}
